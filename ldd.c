975291eca26c89005c676b4a6d31f187983b81f8
shovan2 <shovan@reekon.tools>
2026-04-05 10:09:12 -0400

    proc

--- ldd.c
+++ ldd.c
@@ -1,21 +1,34 @@
 #include <linux/init.h>
 #include <linux/module.h>
-
+#include <linux/proc_fs.h>
 
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Shovan");
 MODULE_DESCRIPTION("First Kernel");
 
+/*
+extern void proc_remove(struct proc_dir_entry *);
+struct proc_dir_entry *proc_create(const char *name, umode_t mode, struct proc_dir_entry *parent, const struct proc_ops *proc_ops);
+proc_ops
+*/ 
+
+static struct proc_dir_entry *custom_proc_node;
+
+
 
 static int shov_module_init(void) {
-
-    printk("Hello World!\n");
+    printk("Entry:Shov_driver\n");
+    custom_proc_node = proc_create("shov_driver",0,NULL,NULL);
+    
     return 0;
 }
 
 static void shov_module_exit(void) {
-    printk("Bye!\n");
+
+    proc_remove(custom_proc_node);
+          printk("Exit:Shov_driver!\n");
+  
 }
 
 module_init(shov_module_init);
